562c6c6af7ba29babe578116e7a5b86cfde76586
Hassan7675 <hassanakhtar291@gmail.com>
2025-03-01 13:00:03 -0500

    solved leetcode problem 26(removing duplicates)

--- arrays/static_arrays/remove_duplicates.py
+++ arrays/static_arrays/remove_duplicates.py
@@ -1,1 +1,9 @@
-#Remove duplicates from sorted array
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        #create two pointers one that iterates thorugh the loop and the other that stores only unique elements
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
