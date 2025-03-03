748187f64e32a77f78364d676d0259c4a58d4708
Hassan7675 <hassanakhtar291@gmail.com>
2025-03-03 18:03:46 -0500

    solved leetcode question 27(remove element)

--- arrays/static_arrays/remove_element.py
+++ arrays/static_arrays/remove_element.py
@@ -1,2 +1,9 @@
-#removing elements from array
-#FIX
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        #iterate through the loop than check if num is not equal to val if it isnt increment k by one
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
