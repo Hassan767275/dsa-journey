c7ec93fc6986319f0dec39722b9d8a3c167107fc
Hassan7675 <hassanakhtar291@gmail.com>
2025-03-03 18:11:08 -0500

    Solved leetcode question 1929(concatenation of an array)

--- arrays/stack/concatenation_of_array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        return nums + nums
-        # ans = []
-        # for i in range(2):
-        #     for num in nums:
-        #         ans.append(num)
-        # return ans
